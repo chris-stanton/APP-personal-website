8675ace1260501c212d646b80a1c70bc5882b27a
Chris Stanton <cstanton0760@yahoo.com>
2017-05-13 02:15:37 -0500

    email view built out and redirects after submit

--- public/scripts/controllers/email.controller.js
+++ public/scripts/controllers/email.controller.js
@@ -1,15 +1,21 @@
-myApp.controller('EmailController', [function() {
+myApp.controller('EmailController', ['$location', function($location) {
 
   console.log('EmailController running');
 
   var self = this;
 
-  self.sendEmail = function(email) {
-    console.log(email);
-
-// empties inputs after submit
-    self.email = "";
+// redirect to contact view
+  function contactView() {
+    $location.path('/contact');
   }
+
+
+// user sends email
+  self.sendEmail = function(email) {
+    console.log("Submitted Email: ", email);
+
+  contactView();
+}//end of sendEmail()
 
 
 }]);//end of myApp.controller
