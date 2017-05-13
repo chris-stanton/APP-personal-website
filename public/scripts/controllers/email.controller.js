588d57e9daa49f76432fd2f6c0a1d5c0d87456c2
Chris Stanton <cstanton0760@yahoo.com>
2017-05-13 01:40:16 -0500

    email object sends to controller

--- public/scripts/controllers/email.controller.js
+++ public/scripts/controllers/email.controller.js
@@ -1,9 +1,15 @@
 myApp.controller('EmailController', [function() {
 
   console.log('EmailController running');
-  var self = this;
-  self.message = 'angular sourced';
 
+  var self = this;
+
+  self.sendEmail = function(email) {
+    console.log(email);
+
+// empties inputs after submit
+    self.email = "";
+  }
 
 
 }]);//end of myApp.controller
